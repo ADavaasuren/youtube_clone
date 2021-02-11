85181509a926ce4224b73d28dedfea6ceebc584b
ADavaasuren <72510317+ADavaasuren@users.noreply.github.com>
2021-02-11 16:19:30 +0100

    Delete firebase.js

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDjTcrNAQdDqxDbLw9lFH8x99X8Y9M6yRs",
-    authDomain: "clone-34f82.firebaseapp.com",
-    databaseURL: "https://clone-34f82.firebaseio.com",
-    projectId: "clone-34f82",
-    storageBucket: "clone-34f82.appspot.com",
-    messagingSenderId: "131803903133",
-    appId: "1:131803903133:web:e60523e8d364d67a009074"
-};
